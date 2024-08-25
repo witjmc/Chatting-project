d76397ab794d90558c9b0144ae1555966e6289c3
witjmc <106864033+witjmc@users.noreply.github.com>
2024-08-25 23:02:20 +0900

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <router-link to="/">Go to Home</router-link><br />
+        <router-link to="/chat">Go to Chat</router-link> <br />
+        <router-link to="/signup">Go to Signup</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
